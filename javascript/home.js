6ebbb36e85b199f8116274459b1b3fffaf47ff4e
IacobZanoci <114911332+IacobZanoci@users.noreply.github.com>
2023-11-14 20:10:56 +0200

    Update home.js

--- javascript/home.js
+++ javascript/home.js
@@ -10,3 +10,18 @@ toggleBtn.onclick = function() {
     ? 'fa-solid fa-xmark'
     : 'fa-solid fa-bars'
 }
+
+function adjustHeader() {
+    var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+    var welcomeTitle = document.querySelector('.welcome_title');
+
+    if (viewportWidth <= 640) {
+        welcomeTitle.innerHTML = 'Designer & <span class="h1_gradient">Developer</span>';
+    } else {
+        welcomeTitle.innerHTML = 'Designer & Web/Mobile <span class="h1_gradient">Developer</span>';
+    }
+}
+
+window.addEventListener('resize', adjustHeader);
+
+adjustHeader();
